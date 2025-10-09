9e4194e1ea2172c6d516e73ea105d990c834b947
Botir Khaltaev <btrghstk@gmail.com>
2025-10-09 15:32:09 +0100

    Export public models at root level for library consumers

---
+++ models.go
@@ -0,0 +1,17 @@
+package adaptiveproxy
+
+import "github.com/Egham-7/adaptive-proxy/internal/models"
+
+type (
+	ServerConfig         = models.ServerConfig
+	ProviderConfig       = models.ProviderConfig
+	EndpointConfig       = models.EndpointConfig
+	EndpointsConfig      = models.EndpointsConfig
+	FallbackConfig       = models.FallbackConfig
+	CircuitBreakerConfig = models.CircuitBreakerConfig
+	CacheConfig          = models.CacheConfig
+	ModelRouterConfig    = models.ModelRouterConfig
+	RateLimitConfig      = models.RateLimitConfig
+	TimeoutConfig        = models.TimeoutConfig
+	ModelCapability      = models.ModelCapability
+)
